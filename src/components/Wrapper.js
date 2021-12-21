985b93dc2d4d08e025044d66964cea79be8673da
Maxwell Rutherford <javert.hugo@yandex.ru>
2021-12-22 00:05:07 +0300

    fix webpack configs, namely 'file-loader'

---
+++ src/components/Wrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardCover from "./card/CardCover";
+import "../index.scss"
+
+const Wrapper = () => (
+    <div className="wrapper">
+        <CardCover />
+    </div>
+);
+
+export default Wrapper;
---
+++ src/components/card/CardCover.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/card.scss";
+import Dune from "../../assets/img/dune.jpg"
+
+
+const CardCover = () => {
+    return (
+        <div className="card-cover">
+            123
+            <img src={Dune} />
+        </div>
+    );
+};
+
+export default CardCover;
