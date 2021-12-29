40c0f71c2b22142f20a4b5222585d7d40afe898d
Maxwell Rutherford <javert.hugo@yandex.ru>
2021-12-29 19:48:56 +0300

    add inner form

--- src/components/card/CardCover.js
+++ src/components/card/CardCover.js
@@ -1,4 +1,5 @@
 import React from "react";
+import InnerForm from "./forms/InnerForm";
 import "../../styles/card.scss";
 import Visa from "../../assets/img/visa-logo-1.png"
 import Chip from "../../assets/img/chip-2.png"
@@ -8,6 +9,7 @@ const CardCover = () => {
         <div className="card-cover">
             <img className="card-chip" src={Chip} />
             <img className="visa-logo" src={Visa} />
+            <InnerForm />
         </div>
     );
 };
---
+++ src/components/card/forms/FourCardNumbers.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const FourCardNumber = () => (
+    <Fragment>
+        <span className="card-items__number__group-count">#</span>
+        <span className="card-items__number__group-count">#</span>
+        <span className="card-items__number__group-count">#</span>
+        <span className="card-items__number__group-count">#</span>
+    </Fragment>
+);
+
+export default FourCardNumber;
