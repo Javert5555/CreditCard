802fb80fbf169c0dea4b0d70107c7a13b0b88ed3
Maxwell Rutherford <javert.hugo@yandex.ru>
2021-12-30 17:36:51 +0300

    add inner form

--- src/components/card/CardCover.js
+++ src/components/card/CardCover.js
@@ -1,12 +1,12 @@
 import React from "react";
-import InnerForm from "./forms/InnerForm";
+import InnerForm from "./InnerForm/InnerForm";
 import "../../styles/card.scss";
 import Visa from "../../assets/img/visa-logo-1.png"
 import Chip from "../../assets/img/chip-2.png"
 
 const CardCover = () => {
     return (
-        <div className="card-cover">
+        <div className="card">
             <img className="card-chip" src={Chip} />
             <img className="visa-logo" src={Visa} />
             <InnerForm />
