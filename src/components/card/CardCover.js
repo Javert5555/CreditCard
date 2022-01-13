91070db5610eca97f1fb82371724041dfebf8796
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-01-13 21:48:08 +0300

    add cvv code handler and make animation of card rotating

--- src/components/card/CardCover.js
+++ src/components/card/CardCover.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import InnerFrontForm from "./inner_form/InnerFrontForm";
 import "../../styles/card.scss";
 import Visa from "../../assets/img/visa-logo-1.png"
 import Chip from "../../assets/img/chip-2.png"
 
-const CardCover = ({ cardNumber, cardHolder, cardMonth }) => {
+const CardCover = ({ cardNumber, cardHolder, cardMonth, cardYear, cardCVV }) => {
+
+    useEffect(() => {
+        let codeCVVElement = document.querySelector(".card-back__cvv-symbols");
+        codeCVVElement.innerHTML = cardCVV.replace(/\d/g, "*");
+    }, [cardCVV])
+
     return (
         <div className="card">
             <div className="card-front">
@@ -14,6 +20,7 @@ const CardCover = ({ cardNumber, cardHolder, cardMonth }) => {
                     cardNumber={cardNumber}
                     cardHolder={cardHolder}
                     cardMonth={cardMonth}
+                    cardYear={cardYear}
                 />
             </div>
             <div className="card-back">
@@ -22,12 +29,7 @@ const CardCover = ({ cardNumber, cardHolder, cardMonth }) => {
                 <div className="card-back__cvv">
                     <p className="card-back__cvv-title">cvv</p>
                     <div className="card-back__cvv-strip">
-                        <div className="card-back__cvv-symbols">
-                            <span className="card-back__cvv-symbol">*</span>
-                            <span className="card-back__cvv-symbol">*</span>
-                            <span className="card-back__cvv-symbol">*</span>
-                            <span className="card-back__cvv-symbol">*</span>
-                        </div>
+                        <div className="card-back__cvv-symbols">{cardCVV}</div>
                     </div>
                     <img className="visa-logo visa-logo_back" src={Visa} />
                 </div>
